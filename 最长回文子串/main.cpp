c73d2864aeec383d4919c9076cfd6124b3f140d9
SeacenLiu <1016667972@qq.com>
2018-11-28 15:17:51 +0800

    最长回文子串

--- 最长回文子串/main.cpp
+++ 最长回文子串/main.cpp
@@ -38,33 +38,27 @@ public:
         int longest = 1;
         int start = 0;
         vector<vector<int>> dp(len, vector<int>(len));
-        for (int i = 0; i < len; i++)
-        {
+        for (int i = 0; i < len; ++i) {
             dp[i][i] = 1;
-            if (i < len-1)
-            {
-                if (s[i] == s[i+1])
-                {
+            if (i<len-1) {
+                if (s[i] == s[i+1]) {
                     dp[i][i+1] = 1;
                     start = i;
                     longest = 2;
                 }
             }
         }
-        for (int l = 3; l <= len; l++) //子串长度
-        {
-            for (int i = 0; i+l-1 < len; i++) // 枚举子串w起始点
-            {
-                int j = l+i+1; // 终点
-                if (s[i] == s[j] && dp[i+1][j-1] == 1)
-                {
+        for (int l = 3; l <= len; ++l) { // 子串长度
+            for (int i = 0; i+l-1 < len; ++i) { // 枚举子串的起始点
+                int j = l + i - 1; // 终点
+                if (s[i] == s[j] && dp[i+1][j-1] == 1) {
                     dp[i][j] = 1;
                     start = i;
                     longest = l;
                 }
             }
         }
-        return s.substr(start,longest);
+        return s.substr(start, longest);
     }
 };
 
@@ -81,6 +75,47 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+/* 有问题的解法
+ class Solution {
+ public:
+ string longestPalindrome(string s) {
+ if (s.empty()) return "";
+ int len = (int)s.size();
+ if (len == 1) return s;
+ int longest = 1;
+ int start = 0;
+ vector<vector<int>> dp(len, vector<int>(len));
+ for (int i = 0; i < len; i++)
+ {
+ dp[i][i] = 1;
+ if (i < len-1)
+ {
+ if (s[i] == s[i+1])
+ {
+ dp[i][i+1] = 1;
+ start = i;
+ longest = 2;
+ }
+ }
+ }
+ for (int l = 3; l <= len; l++) //子串长度
+ {
+ for (int i = 0; i+l-1 < len; i++) // 枚举子串w起始点
+ {
+ int j = l+i+1; // 终点
+ if (s[i] == s[j] && dp[i+1][j-1] == 1)
+ {
+ dp[i][j] = 1;
+ start = i;
+ longest = l;
+ }
+ }
+ }
+ return s.substr(start,longest);
+ }
+ };
+ */
 
 /* 超时啊 暴力解法
  时间复杂度: (n / 2) * n * (n(n+1)/2) => n^3 => O(n^3)
